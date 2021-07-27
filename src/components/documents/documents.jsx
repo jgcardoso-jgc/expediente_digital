8d3ba738b4505b0688350230abb1ff6e2989d1f8
sofia <sofia>
2021-07-27 02:25:14 -0500

    asyncincode

--- src/components/documents/documents.jsx
+++ src/components/documents/documents.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-use-before-define */
 /* eslint-disable operator-linebreak */
 /* eslint-disable prefer-template */
 /* eslint-disable comma-dangle */
@@ -34,66 +35,94 @@ function Documents() {
     contentType: "image/jpeg",
   };
 
+  function exists(response) {
+    if (response.includes("404", 0)) {
+      notExists();
+    } else {
+      response.forEach((url) => {
+        const frontId = new Image();
+        frontId.src = url;
+        frontId.style.width = "100%";
+        frontId.style.borderTopLeftRadius = "14px";
+        frontId.style.borderTopRightRadius = "14px";
+        setLoading(false);
+        document.getElementById("ineFront").appendChild(frontId);
+      });
+    }
+  }
+
+  function notExists() {
+    const script = document.createElement("script");
+    script.src = "https://sdk-js.s3.amazonaws.com/sdk/onBoarding-1.30.1.js";
+    document.body.appendChild(script);
+    script.onload = () => {
+      start();
+      incode
+        .createSession("ALL", null, {
+          configurationId: "60f0969272a9270015196d70",
+        })
+        .then(async () => {
+          try {
+            const imgs = await incode.getImages({
+              token: user.token,
+              body: { images: ["croppedFrontID", "croppedBackID"] },
+            });
+            const keys = Object.keys(imgs);
+            const promises = [];
+            keys.forEach((key) => {
+              const frontId = new Image();
+              frontId.src = `data:image/png;base64,${imgs[key]}`;
+              frontId.style.width = "100%";
+              frontId.style.borderTopLeftRadius = "14px";
+              frontId.style.borderTopRightRadius = "14px";
+              promises.push(
+                db
+                  .ref("users")
+                  .child("/" + user.email + "/" + key)
+                  .putString(imgs[key], "base64", metadata)
+                  .then((res) => {
+                    console.log("uploaded");
+                    setLoading(false);
+                    document.getElementById("ineFront").appendChild(frontId);
+                  })
+              );
+            });
+            await Promise.all(promises).then(console.log("all done"));
+          } catch (e) {
+            toast("Ocurrió un error." + e);
+          }
+        });
+    };
+  }
+
   function getState() {
     console.log(user.token);
+    const docsIncode = ["croppedFrontID", "croppedBackID"];
     if (user.onboarding) {
       if (user.token !== "") {
         setAccess(true);
-        const route = "users/" + user.token + "/frontId";
-        if (
-          db
-            .ref(route)
-            .getDownloadURL()
-            .then((response) => {
-              console.log("exists");
-              console.log(response);
-              const frontId = new Image();
-              frontId.src = response;
-              frontId.style.width = "100%";
-              setLoading(false);
-              document.getElementById("ineFront").appendChild(frontId);
-            })
-            .catch((err) => {
-              console.log("nothing...");
-              const script = document.createElement("script");
-              script.src =
-                "https://sdk-js.s3.amazonaws.com/sdk/onBoarding-1.30.1.js";
-              document.body.appendChild(script);
-              script.onload = () => {
-                console.log("loaded...");
-                start();
-                incode
-                  .createSession("ALL", null, {
-                    configurationId: "60f0969272a9270015196d70",
-                  })
-                  .then(async () => {
-                    try {
-                      const imgs = await incode.getImages({
-                        token: user.token,
-                        body: { images: ["fullFrameFrontID"] },
-                      });
-                      //console.log(imgs.fullFrameFrontID);
-                      const frontId = new Image();
-                      frontId.src = `data:image/png;base64,${imgs.fullFrameFrontID}`;
-                      frontId.style.width = "100%";
-                      //console.log("image:" + image);
-                      db.ref("users")
-                        .child("/" + user.token + "/frontId")
-                        .putString(imgs.fullFrameFrontID, "base64", metadata)
-                        .then((res) => {
-                          console.log("uploaded");
-                          setLoading(false);
-                          document
-                            .getElementById("ineFront")
-                            .appendChild(frontId);
-                        });
-                    } catch (e) {
-                      toast("Ocurrió un error.");
-                    }
-                  });
-              };
-            })
-        );
+        const urls = [];
+        const promises = [];
+        docsIncode.forEach((doc) => {
+          const route = "users/" + user.email + "/" + doc;
+          promises.push(
+            db
+              .ref(route)
+              .getDownloadURL()
+              .then((response) => {
+                urls.push(response);
+                console.log("founded");
+              })
+              .catch((err) => {
+                urls.push("404");
+              })
+          );
+        });
+        Promise.all(promises).then(() => {
+          console.log("all resolved");
+          console.log(urls);
+          exists(urls);
+        });
         return;
       }
       setOnboarding(true);
@@ -143,11 +172,21 @@ function Documents() {
         <h1 className="center pt40 mb20">Mis documentos</h1>
         <div>
           {" "}
-          <div className="container">
+          <div className="container pb40 pt20">
             {loading ? (
               <div className="center pb10"> Cargando tus documentos...</div>
             ) : (
-              <div id="ineFront" className="idFront" />
+              <div>
+                <div className="row">
+                  <div className="col">
+                    <div className="cardFit">
+                      <div id="ineFront" className="idFront" />
+                      <h2 className="cardTitle">INE</h2>
+                      <p className="cardText">Frontal</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
             )}
           </div>
         </div>
