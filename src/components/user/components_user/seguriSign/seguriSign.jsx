b12ea59dd4a8f46eca8384ac9e9542f1f7f02e2f
Emiliano Hidalgo <ehidalgo@sgdata.com>
2021-08-24 22:43:57 -0500

    sign render fix

--- src/components/user/components_user/seguriSign/seguriSign.jsx
+++ src/components/user/components_user/seguriSign/seguriSign.jsx
@@ -1,13 +1,11 @@
-/* eslint-disable comma-dangle */
-/* eslint-disable quotes */
-import React, { useEffect, useRef, useState } from "react";
-import Card from "react-bootstrap/Card";
-import { ToastContainer } from "react-toastify";
-import { ReactSession } from "react-client-session";
-import SegurisignController from "./controller/segurisign_controller";
-import "./segurisign.css";
-import SegurisignDocuments from "./SegurisignDocuments/SegurisignDocuments";
-import CustomToasts from "../Toasts/CustomToasts";
+import React, { useEffect, useRef, useState } from 'react';
+import Card from 'react-bootstrap/Card';
+import { ToastContainer } from 'react-toastify';
+import { ReactSession } from 'react-client-session';
+import SegurisignController from './controller/segurisign_controller';
+import './segurisign.css';
+import SegurisignDocuments from './SegurisignDocuments/SegurisignDocuments';
+import CustomToasts from '../Toasts/CustomToasts';
 
 const seguriSignController = new SegurisignController();
 
@@ -17,7 +15,7 @@ const Segurisign = () => {
   const [logged, setLogged] = useState(false);
   const [loadedCache, setLoadedCache] = useState(false);
   const [loading, setLoading] = useState(true);
-  ReactSession.setStoreType("sessionStorage");
+  ReactSession.setStoreType('sessionStorage');
 
   const signIn = (e) => {
     setLoading(true);
@@ -27,8 +25,8 @@ const Segurisign = () => {
       .then((value) => {
         setLogged(value);
         ReactSession.set(
-          "sign-user",
-          JSON.stringify(seguriSignController.segurisignUser)
+          'sign-user',
+          JSON.stringify(seguriSignController.segurisignUser),
         );
       })
       .catch((error) => {
@@ -40,7 +38,7 @@ const Segurisign = () => {
 
   useEffect(() => {
     if (!loadedCache) {
-      const cookie = ReactSession.get("sign-user");
+      const cookie = ReactSession.get('sign-user');
       if (cookie) {
         seguriSignController.segurisignUser = JSON.parse(cookie);
         setLogged(true);
@@ -50,46 +48,44 @@ const Segurisign = () => {
     }
   }, [loadedCache]);
 
+  if (logged) {
+    return (
+      <SegurisignDocuments seguriSignController={seguriSignController} />
+    );
+  }
+  if (loading) {
+    return <div>Cargando...</div>;
+  }
+
   return (
     <div>
       <ToastContainer />
-      {() => {
-        if (logged) {
-          return (
-            <SegurisignDocuments seguriSignController={seguriSignController} />
-          );
-        }
-        if (loading) {
-          return <div>Cargando...</div>;
-        }
-        return (
-          <div className="centered box-shadow">
-            <Card style={{ width: "18rem" }}>
-              <Card.Header as="h5">Ingresa tu contraseña</Card.Header>
-              <Card.Body className="box-shadow">
-                <Card.Text>
-                  <div>
-                    <form>
-                      <input
-                        className="input-password-sign"
-                        ref={passwordRef}
-                        type="password"
-                      />
-                    </form>
-                  </div>
-                </Card.Text>
-                <button
-                  type="button"
-                  className="btn-seguridata"
-                  onClick={signIn}
-                >
-                  Entrar
-                </button>
-              </Card.Body>
-            </Card>
-          </div>
-        );
-      }}
+      <div className="centered box-shadow">
+        <Card style={{ width: '18rem' }}>
+          <Card.Header as="h5">Ingresa tu contraseña</Card.Header>
+          <Card.Body className="box-shadow">
+            <Card.Text>
+              <div>
+                <form>
+                  <input
+                    className="input-password-sign"
+                    ref={passwordRef}
+                    type="password"
+                  />
+                </form>
+              </div>
+            </Card.Text>
+            <button
+              type="button"
+              className="btn-seguridata"
+              onClick={signIn}
+            >
+              Entrar
+            </button>
+          </Card.Body>
+        </Card>
+      </div>
+      );
     </div>
   );
 };
