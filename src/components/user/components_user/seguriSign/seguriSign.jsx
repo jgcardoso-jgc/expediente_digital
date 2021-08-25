28b37ed44607183c65eee2346546fefe16bce1b0
sofia <sofia>
2021-08-25 02:24:52 -0500

    segurisignui

--- src/components/user/components_user/seguriSign/seguriSign.jsx
+++ src/components/user/components_user/seguriSign/seguriSign.jsx
@@ -1,11 +1,140 @@
-import React, { useEffect, useRef, useState } from 'react';
-import Card from 'react-bootstrap/Card';
-import { ToastContainer } from 'react-toastify';
-import { ReactSession } from 'react-client-session';
-import SegurisignController from './controller/segurisign_controller';
-import './segurisign.css';
-import SegurisignDocuments from './SegurisignDocuments/SegurisignDocuments';
-import CustomToasts from '../Toasts/CustomToasts';
+/* eslint-disable comma-dangle */
+/* eslint-disable quotes */
+import React, { useEffect, useRef, useState } from "react";
+import Card from "react-bootstrap/Card";
+import { createUseStyles } from "react-jss";
+import { ToastContainer } from "react-toastify";
+import { ReactSession } from "react-client-session";
+import SegurisignController from "./controller/segurisign_controller";
+import SegurisignDocuments from "./SegurisignDocuments/SegurisignDocuments";
+import CustomToasts from "../Toasts/CustomToasts";
+import locked from "../../../../assets/locked.png";
+import styles from "../../../../resources/theme";
+
+const globalTheme = createUseStyles(styles);
+const useStyles = createUseStyles(() => ({
+  sigCanvas: { backgroundColor: "white", border: "2px solid black" },
+  sigNewDoc: {
+    backgroundColor: "white",
+    width: "fit-content",
+    alignContent: "center",
+  },
+  centered: {
+    height: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  btnSeguridata: {
+    backgroundColor: "#83bb04",
+    color: "white",
+    height: "1.8rem",
+    boxShadow:
+      "0 4px 4px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+    width: "50%",
+    border: "none",
+    borderRadius: "0.25rem",
+  },
+  btnSeguridataLg: {
+    backgroundColor: "#83bb04",
+    color: "white",
+    boxShadow:
+      "0 4px 16px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+    border: "none",
+    padding: "0.5rem",
+    borderRadius: "0.25rem",
+  },
+  inputStyle: {
+    width: "100%",
+    border: "0",
+    borderBottom: "1px solid rgb(194, 194, 194)",
+    fontSize: "16px",
+    background: "transparent",
+  },
+  card: {
+    backgroundColor: "#f5f5f5",
+    border: `1px solid #f5f5f5`,
+    borderRadius: 10,
+    WebkitBoxShadow: "0px 8px 15px 3px #D1D1D1",
+    boxShadow: "0px 8px 15px 3px #D1D1D1",
+    height: "100%",
+    padding: 20,
+  },
+  inner: { width: "80%", margin: "0 auto", marginTop: "3rem" },
+  accordionButton: {
+    "&:focus": {
+      borderColor: "#83bb04 !important",
+      boxShadow: "none !important",
+    },
+    "&not(.collapsed)": {
+      backgroundColor: "white !important",
+      color: "#83bb04 !important",
+    },
+  },
+  inputPasswordSign: {
+    padding: "0.3rem",
+    borderRadius: "0.2rem",
+    height: "2rem",
+  },
+  inputEmailFirmante: {
+    "&:focus": {
+      borderColor: "#88be0f !important",
+      borderStyle: "solid",
+      borderWidth: "0.01rem",
+    },
+    width: "18rem",
+    display: "block",
+    height: "2.5rem",
+    borderRadius: "0.3rem",
+    borderStyle: "solid",
+    boxShadow:
+      "0 2px 6px 0 rgba(0, 0, 0, 0.2), 0 9px 22px 0 rgba(0, 0, 0, 0.19)",
+    padding: "1rem",
+  },
+  reactPdfPageCanvas: {
+    margin: "0 auto",
+    width: "30% !important",
+    height: "90% !important",
+    marginTop: "2rem",
+    marginBottom: "2rem",
+  },
+  btnSeguridataUpload: {
+    backgroundColor: "white",
+    borderColor: "#88be0f",
+    borderWidth: "0.09rem",
+    borderRadius: "1rem",
+    padding: "0.9rem",
+    boxShadow:
+      "0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 4px 10px 0 rgba(0, 0, 0, 0.19)",
+  },
+  inputCancel: {
+    width: "20rem",
+    height: "15rem",
+    padding: "1rem",
+    margin: "2rem",
+    borderRadius: "0.3rem",
+  },
+  btnDelSigner: {
+    "&:hover": { color: "#88be0f" },
+    backgroundColor: "transparent",
+    border: "none",
+  },
+  btnDropdown: { color: "white" },
+  progressBar: {
+    marginLeft: "2rem",
+    marginRight: "2rem",
+    width: "100%",
+    backgroundColor: "#80808047 !important",
+  },
+  lockedImg: {
+    height: 90,
+    width: 180,
+    display: "block",
+    marginRight: "auto",
+    marginLeft: "auto",
+    marginBottom: 30,
+  },
+}));
 
 const seguriSignController = new SegurisignController();
 
@@ -13,9 +142,11 @@ const Segurisign = () => {
   const passwordRef = useRef(null);
   const toaster = new CustomToasts();
   const [logged, setLogged] = useState(false);
+  const global = globalTheme();
   const [loadedCache, setLoadedCache] = useState(false);
   const [loading, setLoading] = useState(true);
-  ReactSession.setStoreType('sessionStorage');
+  ReactSession.setStoreType("sessionStorage");
+  const classes = useStyles();
 
   const signIn = (e) => {
     setLoading(true);
@@ -25,8 +156,8 @@ const Segurisign = () => {
       .then((value) => {
         setLogged(value);
         ReactSession.set(
-          'sign-user',
-          JSON.stringify(seguriSignController.segurisignUser),
+          "sign-user",
+          JSON.stringify(seguriSignController.segurisignUser)
         );
       })
       .catch((error) => {
@@ -38,7 +169,7 @@ const Segurisign = () => {
 
   useEffect(() => {
     if (!loadedCache) {
-      const cookie = ReactSession.get('sign-user');
+      const cookie = ReactSession.get("sign-user");
       if (cookie) {
         seguriSignController.segurisignUser = JSON.parse(cookie);
         setLogged(true);
@@ -49,9 +180,7 @@ const Segurisign = () => {
   }, [loadedCache]);
 
   if (logged) {
-    return (
-      <SegurisignDocuments seguriSignController={seguriSignController} />
-    );
+    return <SegurisignDocuments seguriSignController={seguriSignController} />;
   }
   if (loading) {
     return <div>Cargando...</div>;
@@ -60,32 +189,28 @@ const Segurisign = () => {
   return (
     <div>
       <ToastContainer />
-      <div className="centered box-shadow">
-        <Card style={{ width: '18rem' }}>
-          <Card.Header as="h5">Ingresa tu contraseña</Card.Header>
-          <Card.Body className="box-shadow">
-            <Card.Text>
-              <div>
-                <form>
-                  <input
-                    className="input-password-sign"
-                    ref={passwordRef}
-                    type="password"
-                  />
-                </form>
-              </div>
-            </Card.Text>
-            <button
-              type="button"
-              className="btn-seguridata"
-              onClick={signIn}
-            >
-              Entrar
-            </button>
-          </Card.Body>
+      <div>
+        <Card className={classes.card} style={{ width: "20rem" }}>
+          <img src={locked} className={classes.lockedImg} alt="locked" />
+          <h5>
+            <b>Ingresa tu contraseña</b>
+          </h5>
+          <Card.Text>
+            <div>
+              <form>
+                <input
+                  className={classes.inputStyle}
+                  ref={passwordRef}
+                  type="password"
+                />
+              </form>
+            </div>
+          </Card.Text>
+          <button type="button" className={global.initBt} onClick={signIn}>
+            Entrar
+          </button>
         </Card>
       </div>
-      );
     </div>
   );
 };
