a44e8494449af1d8d0998340b8a335397c0656dd
sofia <sofia>
2021-09-07 12:14:30 -0500

    newcert

--- src/config/onboarding-config.js
+++ src/config/onboarding-config.js
@@ -1,7 +1,7 @@
 /* eslint-disable operator-linebreak */
 /* eslint-disable quotes */
 export const onboardingSDK = {
-  url: "https://sdk-js.s3.amazonaws.com/sdk/onBoarding-1.33.0.js",
+  url: "https://sdk-js.s3.amazonaws.com/sdk/onBoarding-1.35.0.js",
   idConfig: "60f0969272a9270015196d70",
 };
 export default onboardingSDK;
