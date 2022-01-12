03d5746b3ad58a33282568a5b65330e400d866d0
Emiliano Hidalgo <ehidalgo@sgdata.com>
2022-01-12 10:08:32 -0600

    getList

---
+++ src/components/admin/components_admin/form_controller.js
@@ -0,0 +1,26 @@
+class FormController{
+  apiUrl = "http://200.66.66.214:5001/docs";
+
+  header = { "Content-Type": "application/json" };
+
+
+  async getDocumentList() {
+    const requestOptions = {
+      method: "POST",
+      headers: this.header,
+      body: JSON.stringify(),
+    };
+    const response = await fetch(
+      `${this.apiUrl}`,
+      requestOptions
+    );
+
+    if (response.status === 200) {
+      const data = await response.json();
+      const documents= data.documents;
+      return documents;
+      // handle data
+    }
+    return false;
+  }
+}
